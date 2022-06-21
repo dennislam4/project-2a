1616a156ed37bc5435117896348cc3d9b1d00296
dennislam4 <107871794+dennislam4@users.noreply.github.com>
2022-06-21 15:14:34 -0600

    Edited code to meet the required format.

--- average.py
+++ average.py
@@ -3,10 +3,11 @@
 #Date: 6/21/2022
 #Description: Asks the user to input ANY five numbers and then prints the average of those numbers.
 
-num_1 = float(input("Please enter 5 more numbers: "))
-num_2 = float(input("Please enter 4 more numbers: "))
-num_3 = float(input("Please enter 3 more numbers: "))
-num_4 = float(input("Please enter 2 more numbers: "))
-num_5 = float(input("Please enter 1 more number: "))
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
 avg = ((num_1 + num_2 + num_3 +num_4 + num_5) / 5)
-print(f"The average of your numbers entered is : {avg}.")
+print(f"The average of those numbers is: \"{avg}")
